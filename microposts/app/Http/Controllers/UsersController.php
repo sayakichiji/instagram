147f30ad418331c9f5eef5da16635be1a16d3eba
Saya.Kichiji <ec2-user@ip-172-31-1-248.us-east-2.compute.internal>
2018-06-05 06:57:04 +0000

    user pages

---
+++ microposts/app/Http/Controllers/UsersController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\User; // add
+
+class UsersController extends Controller
+{
+    public function index()
+    {
+        $users = User::paginate(10);
+        $user = User::find($id);
+        
+        return view('users.index', [
+            'users' => $users,
+        ]);
+    }
+}
